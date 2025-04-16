59e7e81e97d9680978108f08e0728ede4c006ad8
AlJf-the-Coder <agjafri@up.edu.ph>
2025-04-16 16:25:03 +0800

    add old Kattis solutions and edit .gitignore

--- Kattis/1d-frogger/frogger.cpp
+++ Kattis/1d-frogger/frogger.cpp
@@ -1,19 +1,21 @@
 #include <stdio.h>
+#include <set>
 
 int main(){
     int n, s, m;
     int squares[200];
     scanf("%d %d %d", &n, &s, &m);
-    for (int i = 0; i < s; i++)
+    for (int i = 0; i < n; i++)
         scanf("%d", squares + i);
-    int ok = curr = 1;
+    int ok = 1, curr = s;
     int k, hops = 0;
-    
+    std::set<int> past; 
+
     while (ok && squares[curr - 1] != m){
+        past.insert(curr);
         k = squares[curr - 1];
         curr += k;
-        // also test if looping
-        ok = 1 <= curr && curr <= s;
+        ok = 1 <= curr && curr <= n && past.find(curr) == past.end();
         hops++;
     }
 
@@ -21,9 +23,9 @@ int main(){
         printf("magic\n");
     else if (curr < 1)
         printf("left\n");
-    else if (curr > s)
-        printf("left\n");
-    else if (same)
+    else if (curr > n)
+        printf("right\n");
+    else if (past.find(curr) != past.end())
         printf("cycle\n");
     
     printf("%d\n", hops);
